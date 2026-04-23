b7650703b4aa3129d510535e2d8fba81dae6d370
Daniel <danielmasson040@gmail.com>
2026-04-23 08:16:35 -0300

    Commit de teste

--- src-react/src/pages/Login/index.jsx
+++ src-react/src/pages/Login/index.jsx
@@ -14,7 +14,7 @@ function Login() {
   return (
     <div style={{ padding: '20px', textAlign: 'center' }}>
       <h1>Bem-vindo ao App de Gestão</h1>
-      <p>Insira suas credenciais</p>
+      <p>Insira suas credenciais!</p>
       
       {/* Botão de exemplo para testar a rota */}
       <button onClick={fazerLogin} style={{ padding: '10px 20px', fontSize: '18px' }}>
