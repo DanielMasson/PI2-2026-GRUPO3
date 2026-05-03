4be64b47a6b135b4862d1052ad5130d2f855816e
Daniel <danielmasson040@gmail.com>
2026-05-03 18:27:16 -0300

    Pagina de Login

--- src-react/src/App.jsx
+++ src-react/src/App.jsx
@@ -1,26 +1,26 @@
 import React from 'react';
-// IMPORTANTE: Importar o HashRouter em vez do BrowserRouter
 import { HashRouter, Routes, Route, Navigate } from 'react-router-dom';
 
-// Importando suas telas
-import Login from './pages/Login';
-import Dashboard from './pages/Dashboard';
+import Login          from './pages/Login';
+import Dashboard      from './pages/Dashboard';
+import Register       from './pages/Register';
+import ForgotPassword from './pages/ForgotPassword';
+import VerifyCode     from './pages/VerifyCode';
+import CreatePassword from './pages/CreatePassword';
+
+import './styles/login_global.css';
 
 function App() {
   return (
     <HashRouter>
       <Routes>
-        {/* Rota padrão: redireciona para o login se acessar a raiz */}
-        <Route path="/" element={<Navigate to="/login" />} />
-        
-        {/* Tela de Login */}
-        <Route path="/login" element={<Login />} />
-        
-        {/* Tela Principal (Dashboard) */}
-        <Route path="/dashboard" element={<Dashboard />} />
-        
-        {/* Futuras telas entrarão aqui */}
-        {/* <Route path="/cadastro-animal" element={<CadastroAnimal />} /> */}
+        <Route path="/"                element={<Navigate to="/login" />} />
+        <Route path="/login"           element={<Login />} />
+        <Route path="/dashboard"       element={<Dashboard />} />
+        <Route path="/cadastro"        element={<Register />} />
+        <Route path="/esqueci-senha"   element={<ForgotPassword />} />
+        <Route path="/verificar-codigo" element={<VerifyCode />} />
+        <Route path="/criar-senha"     element={<CreatePassword />} />
       </Routes>
     </HashRouter>
   );
